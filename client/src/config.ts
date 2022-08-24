fbc34daaba54bcb74496d1f9484826bbfae4fea7
Yusuf Kolawole <quadyusuf@gmail.com>
2022-08-24 22:04:50 +0100

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '464s8capk4';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-gjizm0lb.us.auth0.com',
+  clientId: 'huAzc4FK5OwY5EnWJCub3sNNnrR11fEL',
+  callbackUrl: 'http://localhost:3000/callback'
+};
